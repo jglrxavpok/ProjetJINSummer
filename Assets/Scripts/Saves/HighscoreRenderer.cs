a8ce2d4ed7788d1331dd594de444872c117010f6
jglrxavpok <jglrxavpok@gmail.com>
2020-09-12 22:31:31 +0200

    Highscore menu uses same format for score than rest of game

--- Assets/Scripts/Saves/HighscoreRenderer.cs
+++ Assets/Scripts/Saves/HighscoreRenderer.cs
@@ -5,14 +5,13 @@ using UnityEngine.UI;
 namespace JINSummer.Saves {
     public class HighscoreRenderer : MonoBehaviour {
         private Text text;
-        private static readonly string ScoreFormat = "000000000";
 
         private void Start() {
             text = GetComponent<Text>();
             string contents = "";
             for (int i = 0; i < HighScores.Scores.Length; i++) {
                 int score = HighScores.Scores[i];
-                contents += "<color=cyan>"+(i+1)+"</color>" + ": " + score.ToString(ScoreFormat);
+                contents += "<color=cyan>"+(i+1)+"</color>" + ": " + score.ToString(ScoreTracker.format);
                 if (i % 2 != 0) {
                     contents += "\n";
                 } else {
