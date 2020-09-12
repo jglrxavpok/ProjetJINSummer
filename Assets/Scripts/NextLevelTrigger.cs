24dcae92b3b01d03c96a744da59898bc493e0fbc
jglrxavpok <jglrxavpok@gmail.com>
2020-09-12 12:18:25 +0200

    Portal to go to next levels

---
+++ Assets/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JINSummer {
+    public class NextLevelTrigger : MonoBehaviour {
+        public string nextLevelName;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player")) {
+                SceneManager.LoadScene(nextLevelName);
+            }
+        }
+    }
+}
