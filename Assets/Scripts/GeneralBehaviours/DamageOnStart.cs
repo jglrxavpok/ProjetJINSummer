065bc2ab818a17ad1b1973f90b6eb53804c3b1d9
jglrxavpok <jglrxavpok@gmail.com>
2020-09-13 11:39:16 +0200

    Finished level 1

---
+++ Assets/Scripts/GeneralBehaviours/DamageOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace JINSummer.GeneralBehaviours {
+    [RequireComponent(typeof(HealthSystem))]
+    public class DamageOnStart : MonoBehaviour {
+        public int amount;
+
+        private void Update() {
+            // not called in Start to ensure the health system has been initialized
+            HealthSystem healthSystem = GetComponent<HealthSystem>();
+            healthSystem.Damage(amount);
+            Destroy(this);
+        }
+    }
+}
