c29f6b27d103b6d37749a412e9157cf1359d6000
jglrxavpok <jglrxavpok@gmail.com>
2020-09-13 22:10:29 +0200

    Music and aim sprite

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace JINSummer {
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicPlayer : MonoBehaviour {
+
+        private static MusicPlayer instance = null;
+
+        public static MusicPlayer Instance => instance;
+        private static string currentMusic;
+        
+        private AudioSource audioSource;
+        
+        
+        private void Awake() {
+            if (instance != null && instance != this) {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayMusic(string music) {
+            if (currentMusic != music) {
+                audioSource.Stop();
+                AudioClip musicClip = Resources.Load<AudioClip>(music);
+                if (musicClip != null) {
+                    audioSource.clip = musicClip;
+                    audioSource.Play();
+                } else {
+                    Debug.LogError("Failed to load music: "+music);
+                }
+
+                currentMusic = music;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MusicTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace JINSummer {
+    public class MusicTrigger : MonoBehaviour {
+
+        public AudioClip music;
+        
+        private void Start() {
+            MusicPlayer.Instance.PlayMusic("Music/"+music.name);
+        }
+    }
+}
